d216b11e3edb5357d0b1e26695a535ac85f5a360
EPham42747 <ethancpham@gmail.com>
2023-11-11 01:49:10 -0500

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,23 +1,22 @@
 import cv2
 
-class Capture:
-    def open_capture(self):
-        capture = cv2.VideoCapture(0)                                   # Open webcam
+def open_capture(self):
+    capture = cv2.VideoCapture(0)                                   # Open webcam
 
-        if not capture.isOpened():                                      # Check if webcam was opened correctly
-            print("Error: Could not open webcam.")
-            return None
+    if not capture.isOpened():                                      # Check if webcam was opened correctly
+        print("Error: Could not open webcam.")
+        return None
         return capture
     
-    def close_capture(self, capture):
-        capture.release()                                               # Release the webcam, close  window
-        cv2.destroyAllWindows()
+def close_capture(self, capture):
+    capture.release()                                               # Release the webcam, close  window
+    cv2.destroyAllWindows()
 
-    def get_frame(self, capture):
-        was_read, frame = capture.read()
+def get_frame(self, capture):
+    was_read, frame = capture.read()
 
-        if not was_read:                                                # Check if frame was read successfully
-            print("Error: Could not read frame.")
-            return None
+    if not was_read:                                                # Check if frame was read successfully
+        print("Error: Could not read frame.")
+        return None
 
-        return frame
+    return frame
